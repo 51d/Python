2b54c8539836ed07fba348d1fcaf2f9377426ce2
Siddharth Shorya <shoryasiddharth90@gmail.com>
2017-07-01 01:08:42 +0530

    Update take a break.py

--- take a break.py
+++ take a break.py
@@ -1,5 +1,8 @@
+# this python program reminds a user to take break while working
 import time
 import webbrowser
 print "prog started at" + time.ctime
-time.sleep(10)
-webbrowser.open("http://www.youtube.com/watch?v=dQw4w9WgXcQ")
+break_count = 5
+for i in break_count:
+  time.sleep(60)
+  webbrowser.open("http://www.youtube.com/watch?v=dQw4w9WgXcQ")
